525e94acf8b7997e6871f798e82478bfe3d15a3d
Jorge <jorgegp_95@gmail.com>
2015-10-07 05:41:55 -0500

    Programación Mover Objeto
    se creo la funcionalidad de mover un objeto en el terreno de juego

---
+++ JuegoChagas/Assets/JuegoChagas/Scripts/MoverObjeto.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoverObjeto : MonoBehaviour {
+
+	private Vector3 puntero;
+
+	public void OnMouseDrag()
+	{
+
+		Ray ray= Camera.main.ScreenPointToRay (Input.mousePosition);
+		puntero = ray.origin;
+		puntero.y= transform.position.y;
+		
+
+		Debug.Log ("direccion" + puntero.x + "-" + puntero.y + "-" + puntero.z);
+		this.transform.position = puntero;
+	}
+}
